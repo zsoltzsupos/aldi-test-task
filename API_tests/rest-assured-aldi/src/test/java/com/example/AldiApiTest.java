2f6e4ebec3fa3ac37c5adc0a3217015335ea5883
Zsolt Zsupos <zsoltzsupos@Zsolts-Laptop.local>
2025-08-01 12:20:29 +0200

    Set the Base URI for all tests

--- API_tests/rest-assured-aldi/src/test/java/com/example/AldiApiTest.java
+++ API_tests/rest-assured-aldi/src/test/java/com/example/AldiApiTest.java
@@ -1,5 +1,24 @@
-package com.example;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
-public class AldiApiTest {
-    
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+/**
+ * This class contains basic REST Assured tests for the automationexercise.com API.
+ */
+public class UserApiTests {
+
+    /**
+     * This method sets up the base URI for all tests in this class.
+     * The @BeforeAll annotation means it runs once before any test methods.
+     */
+    @BeforeAll
+    public static void setup() {
+        // Set the base URL for all subsequent requests
+        RestAssured.baseURI = "https://reqres.in/api";
+    }
 }
+
